eb9fe6516d940d61207c4980566c0e01f0829c67
sarthak0401 <sarthakbokade413@gmail.com>
2026-01-08 15:36:57 +0530

    Learning the dice game dom manipulation

---
+++ 01-dom/The-pig-game/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById("score--1");
+const diceEL = document.querySelector(".dice");
+
+// Starting condition
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+
+diceEL.classList.add('hidden');
+
+
+
